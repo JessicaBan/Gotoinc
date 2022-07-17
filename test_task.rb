f7abb9c78baa50348281ce07b998ff5bee9075fc
JessicaBan <fofruby@gmail.com>
2022-07-17 11:54:35 +0300

    My first task in Gotoinc

---
+++ test_task.rb
@@ -0,0 +1,31 @@
+# even = 'Gotoinctesttask'.bytes.select{ |number| number.even?}
+# odd = 'Gotoinctesttask'.bytes.select{ |number| number.odd?}
+
+# puts even.pack('c*') + odd.pack('c*')
+
+	
+
+def encrypt(text, n)
+	if text.to_s.empty?
+		puts "значение входящей строки"
+	end
+	# elsif n <= 0
+	# 	puts "значение ВХОДНОЙ строки"
+	# end
+end
+
+	encrypt('wqrtwe', 0)
+
+# def decrypt(encrypyed_text, n)
+	
+# end
+
+
+
+
+
+
+
+
+
+
